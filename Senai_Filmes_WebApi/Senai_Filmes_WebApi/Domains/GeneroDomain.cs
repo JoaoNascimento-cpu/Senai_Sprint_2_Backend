083d8c9a164fec554c751769e43e6c427004aa39
Joao Nascimento <jhonyzito4gb@gmail.com>
2021-03-21 21:35:42 -0300

    first commit

---
+++ Senai_Filmes_WebApi/Senai_Filmes_WebApi/Domains/GeneroDomain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_Filmes_WebApi.Domains
+{
+    public class GeneroDomain
+    {
+        public int idGenero { get; set; }
+        public string nome { get; set; }
+
+    }
+}
