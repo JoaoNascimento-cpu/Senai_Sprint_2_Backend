83b001af4004e9c3b0aa4a06fc738469b6765eaf
Joao Nascimento <jhonyzito4gb@gmail.com>
2021-03-21 21:41:10 -0300

    interface e domains de generos e filmes salva

--- Senai_Filmes_WebApi/Senai_Filmes_WebApi/Domains/GeneroDomain.cs
+++ Senai_Filmes_WebApi/Senai_Filmes_WebApi/Domains/GeneroDomain.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 
 namespace Senai_Filmes_WebApi.Domains
 {
+    /// <summary>
+    /// Classe que representa a entidade/tabela Gêneros
+    /// </summary>
     public class GeneroDomain
     {
         public int idGenero { get; set; }
--- Senai_Filmes_WebApi/Senai_Filmes_WebApi/Interfaces/IGeneroRepository.cs
+++ Senai_Filmes_WebApi/Senai_Filmes_WebApi/Interfaces/IGeneroRepository.cs
@@ -1,11 +1,54 @@
-﻿using System;
+﻿using Senai_Filmes_WebApi.Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Senai_Filmes_WebApi.Interfaces
 {
-    interface IGeneroRepository
+    /// <summary>
+    /// Interface responsável pelo repositório Genero
+    /// </summary>
+    public interface IGeneroRepository
     {
+        //TipoRetorno NomeMetodo(TipodoParametro nomeParametro)
+        /// <summary>
+        /// Esse método lista todos os generos
+        /// </summary>
+        /// <returns>Retorna lista de generos</returns>
+        List<GeneroDomain> listarTodos();
+        
+        /// <summary>
+        /// Busca um genero pelo id
+        /// </summary>
+        /// <param name="id">o id do genero que sera buscado</param>
+        /// <returns>retorna o objeto (genero) que foi buscado pelo id</returns>
+        GeneroDomain BuscarId(int id);
+        
+        /// <summary>
+        /// este metodo cadastra um novo genero
+        /// </summary>
+        /// <param name="novoGenero">informacao que sera cadastrada</param>
+        void Cadastrar(GeneroDomain novoGenero);
+        
+        /// <summary>
+        /// Esse metodo atualiza um genero existente  passando o id pelo corpo da requisicao
+        /// </summary>
+        /// <param name="genero">genero que sera atualizado com novas informacoes</param>
+        void AtualizarIdCorpo(GeneroDomain genero);
+        
+        /// <summary>
+        /// Atualiza o genero existente passando o id pelo URL da requisicao
+        /// </summary>
+        /// <param name="id">id do genero que sera atualizado</param>
+        /// <param name="">obejto genero com as novas informacoes</param>
+        void AtualizarIdUrl(int id, GeneroDomain genero);
+
+        /// <summary>
+        /// Deleta um genero existente
+        /// </summary>
+        /// <param name="id">id do genero que sera deletado</param>
+        void Deletar(int id);
+
     }
 }
