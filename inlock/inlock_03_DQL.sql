fb009dc62e71258b7fbdebcf1732de5edda1cd0f
Joao Nascimento <jhonyzito4gb@gmail.com>
2021-04-20 12:58:06 -0300

    DDL, DML e DQL feitos V2.0

--- inlock/inlock_03_DQL.sql
+++ inlock/inlock_03_DQL.sql
@@ -15,3 +15,10 @@ ON Jogos.idEstudio = Estudios.idEstudio;
 SELECT  Estudios.nomeEstudio, Jogos.nomeJogo FROM Estudios
 LEFT JOIN Jogos
 ON Estudios.idEstudio = Jogos.idJogo
+
+SELECT Jogos.idJogo, Jogos.nomeJogo FROM Jogos
+
+SELECT Estudios.idEstudio, Estudios.nomeEstudio FROM Estudios
+
+SELECT Usuarios.email, Usuarios.senha FROM Usuarios
+
